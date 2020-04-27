fadf3bf8ee81ad06915cacc3fe1f03f66ac57669
Deepika Sampangi <deepikasampangi@Deepikas-MacBook-Pro.local>
2020-04-27 22:44:32 +0530

    Blog page to details

--- dsam_portfolio/blog/urls.py
+++ dsam_portfolio/blog/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 
-from .views import allblogs
+from .views import allblogs, blog_details
 
 urlpatterns = [
-    path('',allblogs, name='blog_home')
+    path('',allblogs, name='blog_home'),
+    path('<int:blog_id>', blog_details, name='blog_details')
 ]
